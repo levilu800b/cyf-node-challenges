e2cf33fe5f6292deff7a68ce7128f1e68d8d17e8
levilu800b <levilu800b@gmail.com>
2023-02-16 17:10:37 +0000

    post request

--- challenge-mailing-list/server/server.js
+++ challenge-mailing-list/server/server.js
@@ -10,62 +10,75 @@ let people = [
 		name: 'students',
 		members: ['chris@techtonica.org', 'hamid@techtonica.org'],
 	},
-    {
-        name: 'alumni',
-        members: ['chris@techtonica.org', 'hamid@techtonica.org'],
-    },
-    {
-        name: 'volunteers',
-        members: ['chris@techtonica.org', 'hamid@techtonica.org'],
-    },
-    {
-        name: 'mentors',
-        members: ['chris@techtonica.org', 'hamid@techtonica.org'],
-    },
-    {
-        name: 'board',
-        members: ['chris@techtonica.org', 'hamid@techtonica.org'],
-    },
+	{
+		name: 'alumni',
+		members: ['chris@techtonica.org', 'hamid@techtonica.org'],
+	},
+	{
+		name: 'volunteers',
+		members: ['chris@techtonica.org', 'hamid@techtonica.org'],
+	},
+	{
+		name: 'mentors',
+		members: ['chris@techtonica.org', 'hamid@techtonica.org'],
+	},
+	{
+		name: 'board',
+		members: ['chris@techtonica.org', 'hamid@techtonica.org'],
+	},
 ];
 
 app.use(express.json());
 
 app.get('/lists', (req, res) => {
-    res.send(people);
+	res.send(people);
 });
 
 app.get('/lists/:name', (req, res) => {
-    const reqName = req.params.name;
-    const person = people.find(person => person.name === reqName);
-    
-    if (person) {
-        res.send(person);
-    } else res.status(404).send('not availble');
+	const reqName = req.params.name;
+	const person = people.find((person) => person.name === reqName);
+
+	if (person) {
+		res.send(person);
+	} else res.status(404).send('not availble');
 });
 
-app.delete('/lists/:name', (req, res) => {
-    const reqName = req.params.name;
-    const person = people.some(person => person.name === reqName);
-
+app.post('/lists', (req, res) => {
+    const { name, members } = req.body;
+    const person = people.find((person) => person.name === name);
     if (person) {
-        people = people.filter(person => person.name !== reqName);
-        res.send('deleted');
-    } else res.status(404).send('not availble for delete');
-});
-
-app.put('/lists/:name', (req, res) => {
-    const reqName = req.params.name;
-    const person = people.find(person => person.name === reqName);
-
-    if (person) {
-        people.map(person => person.name === reqName ? person.members = req.body.members : person);
-        res.send(people);
+        res.status(400).send('already exist');
     } else {
-        people.push({name: reqName, ...req.body});
+        people.push({ name, members });
         res.send(people);
     }
 });
 
+app.delete('/lists/:name', (req, res) => {
+	const reqName = req.params.name;
+	const person = people.some((person) => person.name === reqName);
+
+	if (person) {
+		people = people.filter((person) => person.name !== reqName);
+		res.send('deleted');
+	} else res.status(404).send('not availble for delete');
+});
+
+app.put('/lists/:name', (req, res) => {
+	const reqName = req.params.name;
+	const person = people.find((person) => person.name === reqName);
+
+	if (person) {
+		people.map((person) =>
+			person.name === reqName ? (person.members = req.body.members) : person,
+		);
+		res.send(people);
+	} else {
+		people.push({ name: reqName, ...req.body });
+		res.send(people);
+	}
+});
+
 app.listen(8080, () => {
-    console.log('listening on port 8080');
+	console.log('listening on port 8080');
 });
